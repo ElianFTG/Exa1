fc6810188ff4f8deab91b235905c7a989bdfd1e8
Elian <eliantito234@gmail.com>
2023-03-09 09:25:01 -0600

    Agregado de restriccion del Detalle, debe tener menos de 151 caracteres

--- src/restriccionTituloContenido.js
+++ src/restriccionTituloContenido.js
@@ -1,13 +1,20 @@
 function restriccion(a, b) {
-  const titulo = toString(a);
+  const titulo = String(a);
+  const contenido = String(b);
   if ((a === "" && b === "")) {
+    alert("El titulo y el Detalle estan vacios, por favor ingrese algo en las casillas");
     return;
   } else {
     if (titulo.length() > 50) {
       alert("El titulo debe tener menos de 51 caracteres");
       return;
     } else {
-      return "<p>" + "Titulo: " + a + "Detalle: " + b + "</p>";
+      if (contenido.length() > 150) {
+        alert("El detalle debe tener menos de 151 caracteres");
+        return;
+      } else {
+        return "<p>" + "Titulo: " + a + "Detalle: " + b + "</p>";
+      }
     }
   }
 }
